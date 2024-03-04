b11fe64b85010b56c7ea92c12e35332b639a7e14
Hatwagner F. Miklós <miklos.hatwagner@rtspro.hu>
2024-03-04 15:57:11 +0100

    feat: drawing an ellipse added

---
+++ ea04/kor3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define A 5 // Fel nagytengely (fel fotengely)
+#define B 9 // Fel kistengely
+using namespace std;
+
+int main() {
+  int sor = -A; 
+  while(sor <= A) {
+    int oszlop = -B;
+    while(oszlop <= B) {
+      if((sor*sor)/double(A*A) + (oszlop*oszlop)/double(B*B) <= 1) cout << '*';
+      else cout << ' ';
+      oszlop++;
+    }
+    sor++;
+    cout << '\n';
+  }
+  return 0;
+}
